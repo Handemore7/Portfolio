c125d4740f2c4be7616937e2fbe830a368f22877
Handemore7 <handemore7@gmail.com>
2023-02-06 14:33:02 -0500

    Sanity connection with about section

--- front-end/src/container/About/About.jsx
+++ front-end/src/container/About/About.jsx
@@ -1,29 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion'
+import { urlFor, client } from '../../client'
 
 import { images } from '../../constants'
 
 import './About.scss'
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good developer',
-    imgUrl: images.about01
-  },
-  {
-    title: 'UX/UI',
-    description: 'I am a good developer',
-    imgUrl: images.about02
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good developer',
-    imgUrl: images.about03
-  }
-]
-
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+
+  }, [])
+  
+
+
   return (
     <>
       <h2 className='head-text'>
@@ -37,7 +32,7 @@ const About = () => {
       <div className='app__profiles'>
         {abouts.map((about, i) => (
           <motion.div whileInView={{opacity: 1}} whileHover={{scale: 1.1}} transition={{duration: 0.5, type: 'tween'}} className='app__profile-item' key={about.title + i}>
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
             <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
