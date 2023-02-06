ae8e88027be0962d63c67708b12963dc0f45cc26
Handemore7 <handemore7@gmail.com>
2023-02-06 14:15:58 -0500

    About and Header

--- front-end/src/container/About/About.jsx
+++ front-end/src/container/About/About.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { motion } from 'framer-motion'
+
+import { images } from '../../constants'
+
 import './About.scss'
+
+const abouts = [
+  {
+    title: 'Web Development',
+    description: 'I am a good developer',
+    imgUrl: images.about01
+  },
+  {
+    title: 'UX/UI',
+    description: 'I am a good developer',
+    imgUrl: images.about02
+  },
+  {
+    title: 'Web Design',
+    description: 'I am a good developer',
+    imgUrl: images.about03
+  }
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+        I Know That 
+        <span> Good Dev</span>
+        <br />
+        Means
+        <span> Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, i) => (
+          <motion.div whileInView={{opacity: 1}} whileHover={{scale: 1.1}} transition={{duration: 0.5, type: 'tween'}} className='app__profile-item' key={about.title + i}>
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
